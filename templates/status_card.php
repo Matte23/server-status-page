cfc418cfbb01dcc911039fe562cfd95a9935e868
Matte23 <matteo.schiff23@gmail.com>
2019-08-02 14:33:26 +0200

    Replace Bootstrap framework with a lightweight CSS file taken from it

--- templates/status_card.php
+++ templates/status_card.php
@@ -1,7 +1,6 @@
-<div class="card <?php echo $color ?> text-white">
+<div class="card <?php echo $color ?> text-white mb-3">
     <div class="card-body">
         <span><?php echo $test->name ?></span>
         <span style="float: right"><?php echo $status ?></span>
     </div>
 </div>
-<br>
